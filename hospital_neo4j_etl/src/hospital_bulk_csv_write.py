2c11c65ef2f63e0f4377e0a0d2497f41e68d3096
uraltaf <mte.altaf@gmail.com>
2024-08-26 13:54:54 +0500

    Hospital Agent

---
+++ hospital_neo4j_etl/src/hospital_bulk_csv_write.py
@@ -0,0 +1,65 @@
+import os
+import logging
+from retry import retry
+from neo4j import GraphDatabase
+
+HOSPITALS_CSV_PATH = os.getenv("HOSPITALS_CSV_PATH")
+PAYERS_CSV_PATH = os.getenv("PAYERS_CSV_PATH")
+PHYSICIANS_CSV_PATH = os.getenv("PHYSICIANS_CSV_PATH")
+PATIENTS_CSV_PATH = os.getenv("PATIENTS_CSV_PATH")
+VISITS_CSV_PATH = os.getenv("VISITS_CSV_PATH")
+REVIEWS_CSV_PATH = os.getenv("REVIEWS_CSV_PATH")
+
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s]: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+LOGGER = logging.getLogger(__name__)
+
+# ...
+
+# ...
+
+NODES = ["Hospital", "Payer", "Physician", "Patient", "Visit", "Review"]
+
+def _set_uniqueness_constraints(tx, node):
+    query = f"""CREATE CONSTRAINT IF NOT EXISTS FOR (n:{node})
+        REQUIRE n.id IS UNIQUE;"""
+    _ = tx.run(query, {})
+
+
+@retry(tries=100, delay=10)
+def load_hospital_graph_from_csv() -> None:
+    """Load structured hospital CSV data following
+    a specific ontology into Neo4j"""
+
+    driver = GraphDatabase.driver(
+        NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD)
+    )
+
+    LOGGER.info("Setting uniqueness constraints on nodes")
+    with driver.session(database="neo4j") as session:
+        for node in NODES:
+            session.execute_write(_set_uniqueness_constraints, node)
+    
+
+    LOGGER.info("Loading hospital nodes")
+    with driver.session(database="neo4j") as session:
+        query = f"""
+        LOAD CSV WITH HEADERS
+        FROM '{HOSPITALS_CSV_PATH}' AS hospitals
+        MERGE (h:Hospital {{id: toInteger(hospitals.hospital_id),
+                            name: hospitals.hospital_name,
+                            state_name: hospitals.hospital_state}});
+        """
+        _ = session.run(query, {})
+
+
+if __name__ == "__main__":
+    load_hospital_graph_from_csv()    
